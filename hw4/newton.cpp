de748359d43c7b660fb3a6213f2712a3524a9178
sir-chipsalot <157821464+sir-chipsalot@users.noreply.github.com>
2024-09-06 17:32:20 -0500

    Update newton.cpp
    Just deleted some unnecessary spaces and changed the number back to 2

--- hw4/newton.cpp
+++ hw4/newton.cpp
@@ -4,7 +4,6 @@ using std::cout;
 using std::endl;
 using std::cin;
 
-
 double f(double, float);
 double derivative(double);
 
@@ -13,7 +12,7 @@ int main()
         //create a value of some start
         double guess = 1;
         //have a value of what we want to find the root of i.e: sqrt(2) 2 is are value in the root
-        float  in_root = 99.8375;
+        float  in_root = 2;
 
         //have a value that calculates the difference between the current guess and previous guess
         float gap = 1;
@@ -41,10 +40,7 @@ int main()
         }
 
         cout << "the root of " << in_root << " is " << guess << " and it took " << tries << " tries to find using Newton's method \n";
-
-
-
-
+        
 }
 //takes the guess and changes the value
 double f(double guess, float in_root){return ((guess*guess)-in_root);}
